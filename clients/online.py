a8ded8826894b3347cabb4e9b813fa6cfb3d3159
Geewynn <ekainugodwin@gmail.com>
2021-05-27 15:47:40 +0100

    kafka client method

---
+++ clients/online.py
@@ -0,0 +1,37 @@
+import json
+import csv
+from kafka import KafkaProducer
+import time
+import argparse
+from customer_segmentation_toolkit.data_zoo import download_data_csv
+
+
+columns =['InvoiceNo','StockCode','Description','Quantity','InvoiceDate','UnitPrice','CustomerID','Country']
+
+csv = "raw_live_data.csv"
+data = download_data_csv(
+        f"data/output/01_data_split_offline_online/{csv}"
+    )
+
+
+def producer(arg=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--endpoint', help='kafka bootstrap broker')
+    known_args, _ = parser.parse_known_args(arg)
+
+    producer = KafkaProducer(
+        bootstrap_servers=[known_args.endpoint],
+        api_version=(0,10,0),
+        value_serializer=lambda v: json.dumps(v).encode('utf-8')
+    )
+    for _, row in data.iterrows():
+        print("sending message")
+        producer.send(topic='messages', value=[row[x] for x in columns])
+        print("message sent, waiting 4s")
+        print(time.sleep(4))
+        producer.flush()
+
+
+
+if __name__ == '__main__':
+    producer()
