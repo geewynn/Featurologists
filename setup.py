b7c68d8c7c31998705f052b2c62bd3be2c2b28a3
artem-y <ajuszkowski@gmail.com>
2021-06-10 18:53:51 +0300

    Install toolkit from branch

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from setuptools import find_packages, setup
 
 
+TOOLKIT_BRANCH = (
+    "allow-build-mini-batch"  # Artem Y: I'm currently working in this branch.
+)
+
 setup(
     name="featurologists",
     version="0.0.1",
@@ -8,7 +12,8 @@ setup(
     url="https://github.com/geewynn/Featurologists",
     packages=find_packages(),
     install_requires=[
-        "customer-segmentation-toolkit>=0.0.2",
+        # "customer-segmentation-toolkit>=0.0.2",
+        f"customer-segmentation-toolkit @ https://github.com/artemlops/customer-segmentation-toolkit/archive/{TOOLKIT_BRANCH}.zip",  # noqa
         "xgboost>=1.4.2",
         "lightgbm>=3.2.1",
         "scikit-learn>=0.24.2",
